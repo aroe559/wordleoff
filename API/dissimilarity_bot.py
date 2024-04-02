f652f600c48740b8b323f4c278034e7f285f559d
Nicholas Patel <137871016+Nicholas-Patel@users.noreply.github.com>
2024-04-02 16:48:58 +1300

    Create dissimilarity_bot.py

---
+++ API/dissimilarity_bot.py
@@ -0,0 +1,2 @@
+# This bot takes in a game state S and current guess G, then returns the 'new' wordle answer.
+# It chooses the 'new' answer based on whatever word has the highest dissimilarity score (based on the current clues and guess)
